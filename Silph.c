e704fe4d74f6634645417bf3ca4b5e80ee7216db
MechaFox <aeseligman@gmail.com>
2015-05-19 15:53:51 -0700

    Clean loadPadModules

--- Silph.c
+++ Silph.c
@@ -27,10 +27,6 @@
 #include <draw.h>
 #include <draw3d.h>
 
-#include "Utilities.c"
-#include "square_data.c"
-
-
 // Pad libraries
 #include <sifrpc.h>
 #include <loadfile.h>
@@ -38,6 +34,10 @@
 #include <math.h>
 
 #include <libpad.h>
+
+
+#include "Utilities.c"
+#include "square_data.c"
 
 
 
@@ -50,147 +50,8 @@ extern unsigned char player_0_0[];
 
 
 
-
-// pad_dma_buf is provided by the user, one buf for each pad
-// contains the pad's current state
-static char padBuf[256] __attribute__((aligned(64)));
-
-static char actAlign[6];
-static int actuators;
-
 VECTOR camera_position = { 0.00f, 0.00f, 100.00f, 1.00f };
 VECTOR camera_rotation = { 0.00f, 0.00f,   0.00f, 1.00f };
-
-/*
- * Local functions
- */
-
-/*
- * loadModules()
- */
-void loadModules()
-{
-    int ret;
-
-    ret = SifLoadModule("rom0:SIO2MAN", 0, NULL);
-    if (ret < 0) {
-        SleepThread();
-    }
-
-    ret = SifLoadModule("rom0:PADMAN", 0, NULL);
-    if (ret < 0) {
-        SleepThread();
-    }
-}
-
-int waitPadReady(int port, int slot)
-{
-    int state;
-    int lastState;
-    char stateString[16];
-
-    state = padGetState(port, slot);
-    lastState = -1;
-    while((state != PAD_STATE_STABLE) && (state != PAD_STATE_FINDCTP1)) {
-        if (state != lastState) {
-            padStateInt2String(state, stateString);
-            printf("Please wait, pad(%d,%d) is in state %s\n",
-                   port, slot, stateString);
-        }
-        lastState = state;
-        state=padGetState(port, slot);
-    }
-    return 0;
-}
-
-int initializePad(int port, int slot)
-{
-
-    int ret;
-    int modes;
-    int i;
-
-    waitPadReady(port, slot);
-
-    // How many different modes can this device operate in?
-    // i.e. get # entrys in the modetable
-    modes = padInfoMode(port, slot, PAD_MODETABLE, -1);
-    printf("The device has %d modes\n", modes);
-
-    if (modes > 0) {
-        printf("( ");
-        for (i = 0; i < modes; i++) {
-            printf("%d ", padInfoMode(port, slot, PAD_MODETABLE, i));
-        }
-        printf(")");
-    }
-
-    printf("It is currently using mode %d\n",
-           padInfoMode(port, slot, PAD_MODECURID, 0));
-
-    // If modes == 0, this is not a Dual shock controller
-    // (it has no actuator engines)
-    if (modes == 0) {
-        printf("This is a digital controller?\n");
-        return 1;
-    }
-
-    // Verify that the controller has a DUAL SHOCK mode
-    i = 0;
-    do {
-        if (padInfoMode(port, slot, PAD_MODETABLE, i) == PAD_TYPE_DUALSHOCK)
-            break;
-        i++;
-    } while (i < modes);
-    if (i >= modes) {
-        printf("This is no Dual Shock controller\n");
-        return 1;
-    }
-
-    // If ExId != 0x0 => This controller has actuator engines
-    // This check should always pass if the Dual Shock test above passed
-    ret = padInfoMode(port, slot, PAD_MODECUREXID, 0);
-    if (ret == 0) {
-        printf("This is no Dual Shock controller??\n");
-        return 1;
-    }
-
-    printf("Enabling dual shock functions\n");
-
-    // When using MMODE_LOCK, user cant change mode with Select button
-    padSetMainMode(port, slot, PAD_MMODE_DUALSHOCK, PAD_MMODE_LOCK);
-
-    waitPadReady(port, slot);
-    printf("infoPressMode: %d\n", padInfoPressMode(port, slot));
-
-    waitPadReady(port, slot);
-    printf("enterPressMode: %d\n", padEnterPressMode(port, slot));
-
-    waitPadReady(port, slot);
-    actuators = padInfoAct(port, slot, -1, 0);
-    printf("# of actuators: %d\n",actuators);
-
-    if (actuators != 0) {
-        actAlign[0] = 0;   // Enable small engine
-        actAlign[1] = 1;   // Enable big engine
-        actAlign[2] = 0xff;
-        actAlign[3] = 0xff;
-        actAlign[4] = 0xff;
-        actAlign[5] = 0xff;
-
-        waitPadReady(port, slot);
-        printf("padSetActAlign: %d\n",
-               padSetActAlign(port, slot, actAlign));
-    }
-    else {
-        printf("Did not find any actuators.\n");
-    }
-
-    waitPadReady(port, slot);
-
-    return 1;
-}
-
 
 
 void init_dma()
@@ -307,9 +168,7 @@ int render(canvas *c)
     u32 old_pad = 0;
     u32 new_pad;
 
-    SifInitRpc(0);
-    loadModules();
-    padInit(0);
+    loadPadModules();
 
     port = 0; // 0 -> Connector 1, 1 -> Connector 2
     slot = 0; // Always zero if not using multitap
